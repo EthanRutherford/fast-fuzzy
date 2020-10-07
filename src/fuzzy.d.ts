267f0e1c6d7b47457c1aa826580ab55b69b096e4
Ethan Rutherford <ethanrutherford2@gmail.com>
2020-10-06 18:33:05 -0700

    Add rollup config for type file

--- src/fuzzy.d.ts
+++ src/fuzzy.d.ts
@@ -1,41 +1,41 @@
 declare module 'fast-fuzzy' {
-  interface FuzzyOptions {
-    ignoreCase?: boolean,
-    ignoreSymbols?: boolean,
-    normalizeWhitespace?: boolean,
-    useDamerau?: boolean,
-    useSellers?: boolean,
-    returnMatchData?: boolean,
-  }
+	interface FuzzyOptions {
+		ignoreCase?: boolean,
+		ignoreSymbols?: boolean,
+		normalizeWhitespace?: boolean,
+		useDamerau?: boolean,
+		useSellers?: boolean,
+		returnMatchData?: boolean,
+	}
 
-  interface AdditionalOptions<T> {
-    keySelector?: (s: T) => string | string[],
-    threshold?: number,
-  }
+	interface AdditionalOptions<T> {
+		keySelector?: (s: T) => string | string[],
+		threshold?: number,
+	}
 
-  export type FullOptions<T> = FuzzyOptions & AdditionalOptions<T>;
+	export type FullOptions<T> = FuzzyOptions & AdditionalOptions<T>;
 
-  export interface MatchData<T> {
-    item: T,
-    original: string,
-    key: string,
-    score: number,
-    match: {
-      index: number,
-      length: number,
-    },
-  }
+	export interface MatchData<T> {
+		item: T,
+		original: string,
+		key: string,
+		score: number,
+		match: {
+			index: number,
+			length: number,
+		},
+	}
 
-  export function fuzzy<T extends FuzzyOptions>(term: string, candidate: string, options?: T): T["returnMatchData"] extends true ? MatchData<string> : number;
-  export function search<T extends (string | object), U extends FullOptions<T>>(term: string, candidates: T[], options?: U):
-    U["returnMatchData"] extends true ? MatchData<T>[] : T[];
-  export class Searcher<T extends (string | object), U extends FullOptions<T>> {
-    constructor(candidates?: T[], options?: U);
-    add(...candidates: T[]): void;
-    search<V extends FullOptions<T>>(term: string, options?: V):
-      V["returnMatchData"] extends true ? MatchData<T>[]
-      : V["returnMatchData"] extends false ? T[]
-      : U["returnMatchData"] extends true ? MatchData<T>[]
-      : T[];
-  }
+	export function fuzzy<T extends FuzzyOptions>(term: string, candidate: string, options?: T): T["returnMatchData"] extends true ? MatchData<string> : number;
+	export function search<T extends (string | object), U extends FullOptions<T>>(term: string, candidates: T[], options?: U):
+		U["returnMatchData"] extends true ? MatchData<T>[] : T[];
+	export class Searcher<T extends (string | object), U extends FullOptions<T>> {
+		constructor(candidates?: T[], options?: U);
+		add(...candidates: T[]): void;
+		search<V extends FullOptions<T>>(term: string, options?: V):
+			V["returnMatchData"] extends true ? MatchData<T>[]
+			: V["returnMatchData"] extends false ? T[]
+			: U["returnMatchData"] extends true ? MatchData<T>[]
+			: T[];
+	}
 }
